cd7407e31f2b050ca35d5cda6a7f7d09a1c95c91
Kirill Gasenko <gasenkokirill97@gmail.com>
2019-07-21 20:54:34 +0300

    todey

--- test.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
- 
-fs.appendFile("text.txt", "\nПривет", function(error){
-    if(error) throw error; 
-                 
-    let data = fs.readFileSync("text.txt", "utf8");
-    console.log(data);  
-});
